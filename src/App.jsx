fe7f3efa00feca6f04315bda9396a2999b4befc8
vincy <vincyvince510@gmail.com>
2025-01-30 14:41:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Head from "./Components/head.jsx"
+import Body from "./Components/body.jsx";
+
+export default function App() {
+  return (
+<>
+<Head />
+
+<div className="overall-container">
+
+<Body
+  img="public/vanam.webp"
+  text="Mr.Whiskerson"
+  phoneno="(212) 555-1234"
+  email="mr.whiskerson@catnap.meow"
+/>
+
+<Body
+  img="public/download (1).jpeg"
+  text="Fluffy"
+  phoneno="(212) 777-2345"
+  email="catchFish@myself.meow"
+/>
+
+<Body
+  img="public/meawcool.jpeg"
+  text="Meowson"
+  phoneno="(345) 167-9876"
+  email="purrson@meow.mom"
+/>
+
+<Body
+  img="public/meowfia.jpeg"
+  text="Mr.Meofia"
+  phoneno="(767) 696-5432"
+  email="meowfia@busy.furr-ever"
+/>
+</div></>
+  );
+}
---
+++ src/Components/head.jsx
@@ -0,0 +1,3 @@
+export default function Head() {
+  return <h1 className="heading">Catty website!</h1>;
+}
